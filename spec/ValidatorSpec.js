891e51608e842e501a69b54a3c44fc9fccd829e7
Hannah Bugai <ganna.bugai@yandex.ru>
2017-12-23 01:25:57 +0300

    v.2

--- spec/ValidatorSpec.js
+++ spec/ValidatorSpec.js
@@ -1,105 +1,183 @@
 describe("Validator", function () {
-
-  it("should test multiple fields", function () {
-    var name = 'Sirius Black';
-    var age = '15';
-    var valid1 = new Validator(name).minLength(3);
-    var valid2 = new Validator(age).isInt();
-    var answer = Validator.validate({str: valid1, num: valid2});
-    expect(answer.str.validation).toBe(true);
-    expect(answer.num.validation).toBe(false);
-  });
-    
-  describe("when checking strings", function () {
-
-    it("should indicate that the string is valid", function () {
+  
+  describe("when checking isRequired()", function () {
+    it("should indicate that value is valid", function () {
       var name = 'Sirius Black';
-      var valid = new Validator(name).isRequired().minLength(3).maxLength(200);
-      var answer = Validator.validate({str: valid});
-      expect(answer.str.validation).toBe(true);
+      var valid = new Validator(name).isRequired();
+      var answer = valid.isValid();
+      expect(answer).toBe(true);
     });
       
-    it("should indicate that the string is too short", function () {
-      var name = 'aa';      
-      var valid = new Validator(name).minLength(3); 
-      var answer = Validator.validate({str: valid});    
-      expect(answer.str.validation).toBe(false);    
+    it("should indicate that value is undefined", function () {
+      var name;      
+      var valid = new Validator(name).isRequired(); 
+      var answer = valid.isValid();    
+      expect(answer).toBe(false);    
     });  
       
-    it("should indicate that the string is too long", function () {
-      var name = 'Sirius Black';      
-      var valid = new Validator(name).maxLength(10); 
-      var answer = Validator.validate({str: valid});    
-      expect(answer.str.validation).toBe(false);    
+    it("should indicate that value is null", function () {
+      var name = null;      
+      var valid = new Validator(name).isRequired(); 
+      var answer = valid.isValid();    
+      expect(answer).toBe(false);
     });
       
-    it("should indicate that the string is empty", function () {
+    it("should indicate that value is empty string", function () {
       var name = '';      
       var valid = new Validator(name).isRequired(); 
-      var answer = Validator.validate({str: valid});    
-      expect(answer.str.validation).toBe(false);    
+      var answer = valid.isValid();    
+      expect(answer).toBe(false);
     });  
   });
 
-  describe("when checking emails", function () {
-
-    it("should indicate that the email is valid", function () {
-      var email = 'wolfstar99@smth.com';     
-      var valid = new Validator(email).isRequired().minLength(3).maxLength(200); 
-      var answer = Validator.validate({str: valid});    
-      expect(answer.str.validation).toBe(true);    
+  describe("when checking maxLength()", function () {
+    it("should indicate that string is valid", function () {
+      var name = 'Sirius Black';
+      var valid = new Validator(name).maxLength(100);
+      var answer = valid.isValid();
+      expect(answer).toBe(true);
     });
       
-    it("should indicate that the email is unvalid (wrong symbols)", function () {
-      var email = 'вульфстар@smth.com';     
-      var valid = new Validator(email).isEmail(); 
-      var answer = Validator.validate({str: valid});    
-      expect(answer.str.validation).toBe(false);    
+    it("should indicate that string is not valid", function () {
+      var name = 'ooooo';      
+      var valid = new Validator(name).maxLength(2);
+      var answer = valid.isValid();    
+      expect(answer).toBe(false);    
     });  
-      
-    it("should indicate that the string is unvalid (wrong structure)", function () {
-      var email = 'aaaaaaaaaaaa';      
-      var valid = new Validator(email).isEmail(); 
-      var answer = Validator.validate({str: valid});    
-      expect(answer.str.validation).toBe(false);    
+  });
+
+  describe("when checking minLength()", function () {
+    it("should indicate that string is valid", function () {
+      var name = 'Sirius Black';
+      var valid = new Validator(name).minLength(5);
+      var answer = valid.isValid();
+      expect(answer).toBe(true);
     });
       
-    it("should indicate that the string is empty", function () {
-      var email = '';      
-      var valid = new Validator(email).isRequired(); 
-      var answer = Validator.validate({str: valid});    
-      expect(answer.str.validation).toBe(false);    
+    it("should indicate that string is not valid", function () {
+      var name = 'ooooo';      
+      var valid = new Validator(name).minLength(6);
+      var answer = valid.isValid();    
+      expect(answer).toBe(false);    
+    });  
+  });
+
+  describe("when checking max()", function () {
+    it("should indicate that value is valid", function () {
+      var age = 20;
+      var valid = new Validator(age).max(20);
+      var answer = valid.isValid();
+      expect(answer).toBe(true);
+    });
+      
+    it("should indicate that value is not valid", function () {
+      var age = 120;
+      var valid = new Validator(age).max(100);
+      var answer = valid.isValid();    
+      expect(answer).toBe(false);    
+    });  
+  });
+
+  describe("when checking min()", function () {
+    it("should indicate that value is valid", function () {
+      var age = 20;
+      var valid = new Validator(age).min(14);
+      var answer = valid.isValid();
+      expect(answer).toBe(true);
+    });
+      
+    it("should indicate that value is not valid", function () {
+      var age = 12;
+      var valid = new Validator(age).min(14);
+      var answer = valid.isValid();    
+      expect(answer).toBe(false);    
     });  
   });
     
-  describe("when checking numbers", function () {
+  describe("when checking isEmail()", function () {
+    it("should indicate that email is valid", function () {
+      var email = 'wolfstar78@gmail.com';
+      var valid = new Validator(email).isEmail();
+      var answer = valid.isValid();
+      expect(answer).toBe(true);
+    });
+      
+    it("should indicate that email is not valid (wrong structure)", function () {
+      var email = 'sdasffsa';
+      var valid = new Validator(email).isEmail();
+      var answer = valid.isValid();    
+      expect(answer).toBe(false);    
+    });  
+      
+    it("should indicate that email is not valid (wrong domain)", function () {
+      var email = 'wolfstar78@gmail';
+      var valid = new Validator(email).isEmail();
+      var answer = valid.isValid();    
+      expect(answer).toBe(false);    
+    }); 
+      
+    it("should indicate that email is not valid (not latin)", function () {
+      var email = 'вульфстар78@gmail';
+      var valid = new Validator(email).isEmail();
+      var answer = valid.isValid();    
+      expect(answer).toBe(false);    
+    });   
+  });
 
-    it("should indicate that the number is valid", function () {
-      var age = 15;      
-      var valid = new Validator(age).isInt().min(0).max(100); 
-      var answer = Validator.validate({str: valid});    
-      expect(answer.str.validation).toBe(true);    
+  describe("when checking isInt()", function () {
+    it("should indicate that 20 is number", function () {
+      var age = 20;
+      var valid = new Validator(age).isInt();
+      var answer = valid.isValid();
+      expect(answer).toBe(true);
     });
       
-    it("should indicate that -15 < 0", function () {
-      var age = -15;      
-      var valid = new Validator(age).min(0);  
-      var answer = Validator.validate({str: valid});    
-      expect(answer.str.validation).toBe(false);    
+    it("should indicate that 3.14 is number", function () {
+      var age = 3.14;
+      var valid = new Validator(age).isInt();
+      var answer = valid.isValid();    
+      expect(answer).toBe(true);    
     });  
-      
-    it("should indicate that 800 > 100", function () {
-      var age = 800;      
-      var valid = new Validator(age).max(100); 
-      var answer = Validator.validate({str: valid});    
-      expect(answer.str.validation).toBe(false);    
+    
+    it("should indicate that string is not number", function () {
+      var age = '20';
+      var valid = new Validator(age).isInt();
+      var answer = valid.isValid();    
+      expect(answer).toBe(false);    
+    });    
+  });
+
+  describe("when checking posibility to chain rules together", function () {
+
+    it("should indicate that value is valid for all rules", function () {
+      var name = 'Sirius Black';
+      var valid = new Validator(name).maxLength(100).minLength(5).isRequired();
+      var answer = valid.isValid();
+      expect(answer).toBe(true);
     });
       
-    it("should indicate that it is number", function () {
-      var age = '30';      
-      var valid = new Validator(age).isInt(); 
-      var answer = Validator.validate({str: valid});    
-      expect(answer.str.validation).toBe(false);    
+    it("should indicate that value is not valid (bc of last rule)", function () {
+       var name = 'Sirius Black';
+      var valid = new Validator(name).minLength(5).isRequired().isEmail();
+      var answer = valid.isValid();    
+      expect(answer).toBe(false);    
     });  
-  });     
+    
+    it("should indicate that value is not valid (not bc of last rule)", function () {
+      var age = 20;
+      var valid = new Validator(age).isInt().min(30).max(40);
+      var answer = valid.isValid();    
+      expect(answer).toBe(false);    
+    });    
+    
+
+    it("should indicate that value is not valid (bc of more that one rule)", function () {
+      var name = 'Sirius Black';
+      var valid = new Validator(name).maxLength(100).minLength(30).isEmail();
+      var answer = valid.isValid();    
+      expect(answer).toBe(false);    
+    });          
+  });
+    
+    
 });
--- src/Validator.js
+++ src/Validator.js
@@ -1,99 +1,66 @@
 function Validator(value){
   this.value=value;
-  this.rules={};  
+  this.rules=[];    
 }
 
 Validator.prototype.isRequired = function() {
-  this.rules.isRequired = true;
-    
+ // this.rules.isRequired = true;
+  this.rules.push(function(value){
+    return !(value === undefined || value === null || value === '') 
+  });    
   return this;
 };
-    
+
 Validator.prototype.maxLength = function(length) {
-  this.rules.maxLength = length;
-    
+  this.rules.push(function(value){
+      return (value.length <= length)
+  })
   return this;
 };
     
 Validator.prototype.minLength = function(length) {
-  this.rules.minLength = length;
-    
+  this.rules.push(function(value){
+      return (value.length >= length)
+  })
   return this;
 };
-    
+   
 Validator.prototype.max = function(length) {
-  this.rules.max = length;
-    
-  return this;  
+  this.rules.push(function(value){
+    return (value <= length);  
+  })
+  return this;
 };
 
 Validator.prototype.min = function(length) {
-  this.rules.min = length;
-    
+  this.rules.push(function(value){
+    return (value >= length);  
+  })
   return this;
 };
-  
+
 Validator.prototype.isEmail = function() {
-  this.rules.isEmail = true;
-    
+  var r = /^[\w\.\d-_]+@[\w\.\d-_]+\.\w{2,4}$/i;
+  this.rules.push(function(value){
+    return (value.match(r)) 
+  });
   return this;  
 };
     
 Validator.prototype.isInt = function() {
-  this.rules.isInt = true;
-    
+  this.rules.push(function(value){
+    return (typeof(value) === 'number')
+  });
   return this;  
 };
 
-Validator.validate = function(arr) {
-  var answ={};
-    
-  for (var param in arr) {
-    answ[param] = {validation: true, };    
-    
-    if ('isRequired' in arr[param].rules)   {
-      if (arr[param].value === undefined || arr[param].value === null || arr[param].value === '') {
-        answ[param] = {validation: false, msg:"Параметр не должен быть пустым",}
-      }     
-    }    
-        
-    if ('maxLength' in arr[param].rules) {
-      if (arr[param].value.length > arr[param].rules.maxLength) {
-        answ[param] = {validation: false, msg:"Строка должна быть короче "+arr[param].rules.maxLength};
-      }
-    }     
-        
-    if ('minLength' in arr[param].rules) {
-      if (arr[param].value.length < arr[param].rules.minLength) {
-        answ[param] = {validation: false, msg:"Строка должна быть не короче "+arr[param].rules.minLength};}
-    }    
-        
-    if ('max' in arr[param].rules) {
-      if (arr[param].value > arr[param].rules.max) {
-        answ[param] = {validation: false, msg:"Значение должно быть меньше "+arr[param].rules.max};
-      }
-    } 
- 
-    if ('min' in arr[param].rules) {
-      if (arr[param].value < arr[param].rules.min) {
-        answ[param] = {validation: false, msg:"Значение должно быть больше "+arr[param].rules.min};
-      }
-    }     
-    
-    if ('isEmail' in arr[param].rules) {
-      var r = /^[\w\.\d-_]+@[\w\.\d-_]+\.\w{2,4}$/i;
-      if (!arr[param].value.match(r)) {
-        answ[param] ={validation: false, msg:"Некоректный email",};
-      }     
-    }   
- 
-    if ('isInt' in arr[param].rules)   {
-    //  integerRegex = /^\-?[0-9]+$/    
-      if (typeof(arr[param].value) !== 'number') {
-        answ[param] ={validation: false, msg:"Значение должно быть числом",};
-      } 
-    }
+Validator.prototype.isValid = function() {
+  var answ = true;
+  
+  for (i = 0; i < this.rules.length; i++) {
+    var answer = this.rules[i](this.value);
+    if (!answer) answ = false;  
   }
     
-  return answ;
-};
+  return answ;    
+}
